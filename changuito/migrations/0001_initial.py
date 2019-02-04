386c6122a8c3fbdcd664a7008913b59536b34e97
Kehinde Adewusi <kehinde.adewusi@evincesystems.com>
2019-02-04 13:35:20 +0100

    adds required on_delete behaviour to items model

--- changuito/migrations/0001_initial.py
+++ changuito/migrations/0001_initial.py
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
                 ('creation_date', models.DateTimeField(verbose_name='creation date', default=django.utils.timezone.now)),
                 ('checked_out', models.BooleanField(verbose_name='checked out', default=False)),
-                ('user', models.ForeignKey(blank=True, null=True, to=settings.AUTH_USER_MODEL)),
+                ('user', models.ForeignKey(blank=True, null=True, to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)),
             ],
             options={
                 'verbose_name_plural': 'carts',
@@ -34,8 +34,8 @@ class Migration(migrations.Migration):
                 ('quantity', models.DecimalField(decimal_places=3, verbose_name='quantity', max_digits=18)),
                 ('unit_price', models.DecimalField(decimal_places=2, verbose_name='unit price', max_digits=18)),
                 ('object_id', models.PositiveIntegerField()),
-                ('cart', models.ForeignKey(verbose_name='cart', to='changuito.Cart')),
-                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+                ('cart', models.ForeignKey(verbose_name='cart', to='changuito.Cart', on_delete=models.CASCADE)),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType', on_delete=models.CASCADE)),
             ],
             options={
                 'verbose_name_plural': 'items',
--- changuito/models.py
+++ changuito/models.py
@@ -51,13 +51,13 @@ class ItemManager(models.Manager):
 
 
 class Item(models.Model):
-    cart = models.ForeignKey(Cart, verbose_name=_('cart'))
+    cart = models.ForeignKey(Cart, verbose_name=_('cart'), on_delete=models.CASCADE)
     quantity = models.DecimalField(max_digits=18, decimal_places=3,
                                    verbose_name=_('quantity'))
     unit_price = models.DecimalField(max_digits=18, decimal_places=2,
                                      verbose_name=_('unit price'))
     # product as generic relation
-    content_type = models.ForeignKey(ContentType)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
 
     objects = ItemManager()
